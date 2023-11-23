4200d64476aa0162d9c0501352ec09aacb1a90db
Alexandra Oprea <alexandraoprea>
2023-11-23 16:31:26 +0000

    First commit

---
+++ database/migrations/2023_11_21_181448_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->bool('need_accommodation');
+            $table->datetime('expired_at');
+            $table->double('price');
+            $table->integer('quantity');
+            $table->enum('type', ["type1", "type2"])
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
